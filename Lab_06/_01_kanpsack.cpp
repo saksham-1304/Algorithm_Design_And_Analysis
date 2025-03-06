789d8d1e275ce7dd1b72f432379f06a27f2a4ada
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2025-03-06 14:43:45 +0530

    Lab 06

--- Lab_06/_01_kanpsack.cpp
+++ Lab_06/_01_kanpsack.cpp
@@ -4,33 +4,33 @@
 using namespace std;
 int main()
 {
-    int n, W;
-    cout << "Enter number of items: ";
-    cin >> n;
-    cout << "Enter the capacity of the knapsack: ";
-    cin >> W;
+  int n, W;
+  cout << "Enter number of items: ";
+  cin >> n;
+  cout << "Enter the capacity of the knapsack: ";
+  cin >> W;
 
-    vector<pair<int, int>> items(n);
-    cout << "Enter the weight and value of each item: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> items[i].first >> items[i].second;
-    }
+  vector<pair<int, int>> items(n);
+  cout << "Enter the weight and value of each item: ";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> items[i].first >> items[i].second;
+  }
 
-    sort(items.begin(), items.end(), [](pair<int, int> a, pair<int, int> b)
-         { return (double)a.second / a.first > (double)b.second / b.first; });
+  sort(items.begin(), items.end(), [](pair<int, int> a, pair<int, int> b)
+       { return (double)a.second / a.first > (double)b.second / b.first; });
 
-    double totalValue = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (W == 0)
-            break;
-        int weight = min(W, items[i].first);
-        totalValue += weight * (double)items[i].second / items[i].first;
-        W -= weight;
-    }
+  double totalValue = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (W == 0)
+      break;
+    int weight = min(W, items[i].first);
+    totalValue += weight * (double)items[i].second / items[i].first;
+    W -= weight;
+  }
 
-    cout << "Total value of items in the knapsack: " << totalValue << endl;
+  cout << "Total value of items in the knapsack: " << totalValue << endl;
 
-    return 0;
+  return 0;
 }
--- Lab_06/_02_strassen_s_matrix.cpp
+++ Lab_06/_02_strassen_s_matrix.cpp
@@ -1,135 +1,155 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
-void addMatrix(vector<vector<int>> &A, vector<vector<int>> &B, vector<vector<int>> &C, int size)
+void addMatrix(vector<vector<int>> &A, vector<vector<int>> &B,
+               vector<vector<int>> &C, int size)
 {
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            C[i][j] = A[i][j] + B[i][j];
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++)
+      C[i][j] = A[i][j] + B[i][j];
 }
 
-void subMatrix(vector<vector<int>> &A, vector<vector<int>> &B, vector<vector<int>> &C, int size)
+void subMatrix(vector<vector<int>> &A, vector<vector<int>> &B,
+               vector<vector<int>> &C, int size)
 {
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            C[i][j] = A[i][j] - B[i][j];
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++)
+      C[i][j] = A[i][j] - B[i][j];
 }
 
-void strassen(vector<vector<int>> &A, vector<vector<int>> &B, vector<vector<int>> &C, int size)
+void strassen(vector<vector<int>> &A, vector<vector<int>> &B,
+              vector<vector<int>> &C, int size)
 {
-    if (size == 1)
+  if (size == 1)
+  {
+    C[0][0] = A[0][0] * B[0][0];
+    return;
+  }
+
+  int newSize = size / 2;
+  vector<vector<int>> A11(newSize, vector<int>(newSize));
+  vector<vector<int>> A12(newSize, vector<int>(newSize));
+  vector<vector<int>> A21(newSize, vector<int>(newSize));
+  vector<vector<int>> A22(newSize, vector<int>(newSize));
+  vector<vector<int>> B11(newSize, vector<int>(newSize));
+  vector<vector<int>> B12(newSize, vector<int>(newSize));
+  vector<vector<int>> B21(newSize, vector<int>(newSize));
+  vector<vector<int>> B22(newSize, vector<int>(newSize));
+
+  for (int i = 0; i < newSize; i++)
+    for (int j = 0; j < newSize; j++)
     {
-        C[0][0] = A[0][0] * B[0][0];
-        return;
+      A11[i][j] = A[i][j];
+      A12[i][j] = A[i][j + newSize];
+      A21[i][j] = A[i + newSize][j];
+      A22[i][j] = A[i + newSize][j + newSize];
+
+      B11[i][j] = B[i][j];
+      B12[i][j] = B[i][j + newSize];
+      B21[i][j] = B[i + newSize][j];
+      B22[i][j] = B[i + newSize][j + newSize];
     }
 
-    int newSize = size / 2;
-    vector<vector<int>> A11(newSize, vector<int>(newSize));
-    vector<vector<int>> A12(newSize, vector<int>(newSize));
-    vector<vector<int>> A21(newSize, vector<int>(newSize));
-    vector<vector<int>> A22(newSize, vector<int>(newSize));
+  vector<vector<int>> P1(newSize, vector<int>(newSize));
+  vector<vector<int>> P2(newSize, vector<int>(newSize));
+  vector<vector<int>> P3(newSize, vector<int>(newSize));
+  vector<vector<int>> P4(newSize, vector<int>(newSize));
+  vector<vector<int>> P5(newSize, vector<int>(newSize));
+  vector<vector<int>> P6(newSize, vector<int>(newSize));
+  vector<vector<int>> P7(newSize, vector<int>(newSize));
+  vector<vector<int>> temp1(newSize, vector<int>(newSize));
+  vector<vector<int>> temp2(newSize, vector<int>(newSize));
 
-    vector<vector<int>> B11(newSize, vector<int>(newSize));
-    vector<vector<int>> B12(newSize, vector<int>(newSize));
-    vector<vector<int>> B21(newSize, vector<int>(newSize));
-    vector<vector<int>> B22(newSize, vector<int>(newSize));
+  addMatrix(A11, A22, temp1, newSize);
+  addMatrix(B11, B22, temp2, newSize);
+  strassen(temp1, temp2, P1, newSize);
 
-    for (int i = 0; i < newSize; i++)
-        for (int j = 0; j < newSize; j++)
-        {
-            A11[i][j] = A[i][j];
-            A12[i][j] = A[i][j + newSize];
-            A21[i][j] = A[i + newSize][j];
-            A22[i][j] = A[i + newSize][j + newSize];
+  addMatrix(A21, A22, temp1, newSize);
+  strassen(temp1, B11, P2, newSize);
 
-            B11[i][j] = B[i][j];
-            B12[i][j] = B[i][j + newSize];
-            B21[i][j] = B[i + newSize][j];
-            B22[i][j] = B[i + newSize][j + newSize];
-        }
+  subMatrix(B12, B22, temp1, newSize);
+  strassen(A11, temp1, P3, newSize);
 
-    vector<vector<int>> P1(newSize, vector<int>(newSize));
-    vector<vector<int>> P2(newSize, vector<int>(newSize));
-    vector<vector<int>> P3(newSize, vector<int>(newSize));
-    vector<vector<int>> P4(newSize, vector<int>(newSize));
-    vector<vector<int>> P5(newSize, vector<int>(newSize));
-    vector<vector<int>> P6(newSize, vector<int>(newSize));
-    vector<vector<int>> P7(newSize, vector<int>(newSize));
+  subMatrix(B21, B11, temp1, newSize);
+  strassen(A22, temp1, P4, newSize);
 
-    vector<vector<int>> temp1(newSize, vector<int>(newSize));
-    vector<vector<int>> temp2(newSize, vector<int>(newSize));
+  addMatrix(A11, A12, temp1, newSize);
+  strassen(temp1, B22, P5, newSize);
 
-    addMatrix(A11, A22, temp1, newSize);
-    addMatrix(B11, B22, temp2, newSize);
-    strassen(temp1, temp2, P1, newSize);
+  subMatrix(A21, A11, temp1, newSize);
+  addMatrix(B11, B12, temp2, newSize);
+  strassen(temp1, temp2, P6, newSize);
 
-    addMatrix(A21, A22, temp1, newSize);
-    strassen(temp1, B11, P2, newSize);
+  subMatrix(A12, A22, temp1, newSize);
+  addMatrix(B21, B22, temp2, newSize);
+  strassen(temp1, temp2, P7, newSize);
 
-    subMatrix(B12, B22, temp1, newSize);
-    strassen(A11, temp1, P3, newSize);
+  vector<vector<int>> C11(newSize, vector<int>(newSize));
+  vector<vector<int>> C12(newSize, vector<int>(newSize));
+  vector<vector<int>> C21(newSize, vector<int>(newSize));
+  vector<vector<int>> C22(newSize, vector<int>(newSize));
 
-    subMatrix(B21, B11, temp1, newSize);
-    strassen(A22, temp1, P4, newSize);
+  addMatrix(P1, P4, temp1, newSize);
+  subMatrix(temp1, P5, temp2, newSize);
+  addMatrix(temp2, P7, C11, newSize);
 
-    addMatrix(A11, A12, temp1, newSize);
-    strassen(temp1, B22, P5, newSize);
+  addMatrix(P3, P5, C12, newSize);
+  addMatrix(P2, P4, C21, newSize);
 
-    subMatrix(A21, A11, temp1, newSize);
-    addMatrix(B11, B12, temp2, newSize);
-    strassen(temp1, temp2, P6, newSize);
+  addMatrix(P1, P3, temp1, newSize);
+  subMatrix(temp1, P2, temp2, newSize);
+  addMatrix(temp2, P6, C22, newSize);
 
-    subMatrix(A12, A22, temp1, newSize);
-    addMatrix(B21, B22, temp2, newSize);
-    strassen(temp1, temp2, P7, newSize);
-
-    addMatrix(P1, P4, temp1, newSize);
-    subMatrix(temp1, P5, temp2, newSize);
-    addMatrix(temp2, P7, C, newSize);
+  for (int i = 0; i < newSize; i++)
+    for (int j = 0; j < newSize; j++)
+    {
+      C[i][j] = C11[i][j];
+      C[i][j + newSize] = C12[i][j];
+      C[i + newSize][j] = C21[i][j];
+      C[i + newSize][j + newSize] = C22[i][j];
+    }
 }
 
 int nextPowerOf2(int n)
 {
-    int power = 1;
-    while (power < n)
-        power *= 2;
-    return power;
+  int power = 1;
+  while (power < n)
+    power *= 2;
+  return power;
 }
 
 int main()
 {
-    int n;
-    cout << "Enter matrix size n: ";
-    cin >> n;
+  int n;
+  cout << "Enter matrix size n: ";
+  cin >> n;
 
-    int newSize = nextPowerOf2(n);
+  int newSize = nextPowerOf2(n);
+  vector<vector<int>> A(newSize, vector<int>(newSize, 0));
+  vector<vector<int>> B(newSize, vector<int>(newSize, 0));
+  vector<vector<int>> C(newSize, vector<int>(newSize, 0));
 
-    vector<vector<int>> A(newSize, vector<int>(newSize, 0));
-    vector<vector<int>> B(newSize, vector<int>(newSize, 0));
-    vector<vector<int>> C(newSize, vector<int>(newSize, 0));
+  cout << "Enter matrix A (" << n << "x" << n << "):\n";
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      cin >> A[i][j];
 
-    cout << "Enter matrix A (" << n << "x" << n << "):\n";
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> A[i][j];
+  cout << "Enter matrix B (" << n << "x" << n << "):\n";
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      cin >> B[i][j];
 
-    cout << "Enter matrix B (" << n << "x" << n << "):\n";
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> B[i][j];
+  strassen(A, B, C, newSize);
 
-    strassen(A, B, C, newSize);
+  cout << "Resultant Matrix C (" << n << "x" << n << "):\n";
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+      cout << C[i][j] << " ";
+    cout << endl;
+  }
 
-    cout << "Resultant Matrix C (" << n << "x" << n << "):\n";
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-            cout << C[i][j] << " ";
-        cout << endl;
-    }
-
-    return 0;
+  return 0;
 }
